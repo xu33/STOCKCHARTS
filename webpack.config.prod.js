3404c88c3f4a743de74a2f5f337dcb8533b1e9b4
WHUP\99171 <nan.shi@upchina.com>
2017-10-19 16:53:31 +0800

    update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,48 +3,59 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: {
-    PredictChart: './src/PredictChart.js',
-    ChartWithVolume: './src/ChartWithVolume.js',
-    PredictChartMobile: './src/Predict.js',
-    SampleChart: './src/Sample.js',
-    StockChart: './src/StockChart.js',
-    TimeTrendChart: './src/mobile/TimeTrendChart.js',
-    CandleStickChart: './src/mobile/CandleStickChart',
+    // PredictChart: './src/PredictChart.js',
+    // ChartWithVolume: './src/ChartWithVolume.js',
+    // PredictChartMobile: './src/Predict.js',
+    // SampleChart: './src/Sample.js',
+    // StockChart: './src/StockChart.js',
+    // TimeTrendChart: './src/mobile/TimeTrendChart.js',
+    CandleStickChart: './src/CandleStickCharts/CandleStickChart.js',
     d3: ['d3']
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    // filename: '[name].[chunkhash].js',
+    filename: '[name].js',
     library: '[name]'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loader: 'babel-loader',
-      include: path.join(__dirname, 'src')
-    }, {
-      test: /\.css$/,
-      loader: 'style!css'
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       names: ['d3'],
       minChunks: 3
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-
-      // Eliminate comments
-      comments: false,
-
-      // Compression specific options
-      compress: {
-        // remove warnings
-        warnings: false,
-
-        // Drop console statements
-        drop_console: true
-      }
     })
+    // new webpack.optimize.UglifyJsPlugin({
+    //   // Eliminate comments
+    //   comments: false,
+
+    //   // Compression specific options
+    //   compress: {
+    //     // remove warnings
+    //     warnings: false,
+
+    //     // Drop console statements
+    //     drop_console: true
+    //   }
+    // })
   ]
 };
