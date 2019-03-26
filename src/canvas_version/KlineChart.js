ad3914ab7857a7f940a1454b2e53b7d95cb219e1
WHUP\99171 <nan.shi@upchina.com>
2019-03-26 16:05:33 +0800

    axis

--- src/canvas_version/KlineChart.js
+++ src/canvas_version/KlineChart.js
@@ -1,21 +1,12 @@
-const RED = '#ff3d3d';
-const GREEN = '#0fc351';
+const RED = '#d20';
+const GREEN = '#093';
 const EQUAL = '#999999';
+const GRID_COLOR = '#edf0f5';
+const BLUE = '#07d';
 
 function KlineChart(element, options) {
   let { data } = options;
   let { width, height } = element.getBoundingClientRect();
-
-  // var start = 0;
-  // var end = 100;
-  // var total = 1000;
-
-  // var width = 400;
-
-  // var scale = d3
-  //   .scaleLinear()
-  //   .range([0, width])
-  //   .domain([start, end]);
 
   const canvas = document.createElement('canvas');
   canvas.width = width;
@@ -43,8 +34,7 @@ function KlineChart(element, options) {
     if (startIndex < 0) startIndex = 0;
     if (endIndex > data.length - 1) endIndex = data.length - 1;
 
-    console.log(startIndex, endIndex);
-    computePriceHeightScale();
+    // console.log(startIndex, endIndex);
     renderSticks();
   };
 
@@ -69,8 +59,33 @@ function KlineChart(element, options) {
       .call(zoom.transform, transform);
   };
 
+  const drawRect = (x, y, width, height, fill = true) => {
+    x = parseInt(x) + 0.5;
+    y = parseInt(y) + 0.5;
+    width = parseInt(width) + 0.5;
+    height = parseInt(height) + 0.5;
+    if (fill) {
+      context.fillRect(x, y, width, height);
+    } else {
+      context.strokeRect(x, y, width, height);
+    }
+  };
+
+  const drawLine = (x1, y1, x2, y2) => {
+    x1 = parseInt(x1) + 0.5;
+    y1 = parseInt(y1) + 0.5;
+    x2 = parseInt(x2) + 0.5;
+    y2 = parseInt(y2) + 0.5;
+
+    context.beginPath();
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+
+    context.stroke();
+    context.closePath();
+  };
+
   const computePriceHeightScale = () => {
-    console.log(data);
     let part = data.slice(startIndex, endIndex);
     let min = Math.min.apply(
       Math,
@@ -89,8 +104,93 @@ function KlineChart(element, options) {
     priceHeightScale.domain([min, max]);
   };
 
+  // 价格轴
+  const renderLeftAxis = () => {
+    let priceDomain = priceHeightScale.domain();
+    let [min, max] = priceDomain;
+    let i1 = d3.interpolateNumber(height, 0);
+    let i2 = d3.interpolateNumber(min, max);
+
+    for (let i = 0; i <= 6; i++) {
+      let x = 4;
+      let y = i1(i / 6);
+      let text = i2(i / 6).toFixed(2);
+
+      if (i == 6) {
+        y += 12;
+      } else {
+        y -= 4;
+      }
+      console.log(i, x, y, text);
+
+      context.fillText(text, x, y);
+    }
+  };
+
+  // 辅助线
+  const renderGrids = () => {
+    // let interpolator = d3.interpolateNumber(height, 0);
+    // let range = [];
+    // let lineNumbers = 6;
+    // for (let i = 0; i <= lineNumbers; i++) {
+    //   range.push(interpolator(i / lineNumbers));
+    // }
+
+    // context.save();
+    // context.strokeStyle = GRID_COLOR;
+    // range.forEach(y => {
+    //   drawLine(0, y, width, y);
+    // });
+    // context.restore();
+    renderHozGrids();
+    renderVerGrids();
+  };
+
+  const renderHozGrids = () => {
+    let interpolator = d3.interpolateNumber(height, 0);
+    let range = [];
+    let lineNumbers = 6;
+    for (let i = 0; i <= lineNumbers; i++) {
+      range.push(interpolator(i / lineNumbers));
+    }
+
+    context.save();
+    context.strokeWidth = 2;
+    context.strokeStyle = GRID_COLOR;
+    range.forEach(y => {
+      drawLine(0, y, width, y);
+    });
+    context.restore();
+  };
+
+  const renderVerGrids = () => {
+    let interpolator = d3.interpolateNumber(0, width);
+    let range = [];
+    let lineNumbers = 4;
+    for (let i = 0; i <= lineNumbers; i++) {
+      range.push(interpolator(i / lineNumbers));
+    }
+
+    context.save();
+    context.strokeWidth = 2;
+
+    context.strokeStyle = GRID_COLOR;
+    range.forEach(x => {
+      drawLine(x, 0, x, height);
+    });
+    context.restore();
+  };
+
+  // renderGrids();
+
   const renderSticks = () => {
     context.clearRect(0, 0, width, height);
+
+    // 根据数据范围新计算比例尺
+    computePriceHeightScale();
+    renderGrids();
+    renderLeftAxis();
+
     let part = data.slice(startIndex, endIndex);
     const scale = d3
       .scaleBand()
@@ -99,52 +199,77 @@ function KlineChart(element, options) {
       .padding(0.3);
 
     const rectWidth = scale.bandwidth();
+    // context.globalCompositeOperation = 'destination-out';
+    // context.globalAlpha = 1;
 
-    for (let i = 0; i < part.length; i++) {
+    if (rectWidth < 4) {
       context.save();
-      let { fOpen, fClose, fLow, fHigh } = part[i];
+      context.strokeStyle = BLUE;
+      context.beginPath();
 
-      // 蜡烛
-      let x = scale(i);
-      let y = priceHeightScale(Math.max(fOpen, fClose));
-      let width = rectWidth;
-      let height = Math.abs(priceHeightScale(fOpen) - priceHeightScale(fClose));
-      let color;
+      for (let i = 0; i < part.length; i++) {
+        let { fClose } = part[i];
+        let x = scale(i);
+        let y = priceHeightScale(fClose);
 
-      if (fOpen > fClose) {
-        color = GREEN;
-      } else if (fOpen < fClose) {
-        color = RED;
-      } else {
-        color = EQUAL;
+        if (i === 0) {
+          context.moveTo(x, y);
+        } else {
+          context.lineTo(x, y);
+        }
       }
 
-      context.fillStyle = color;
-      context.strokeStyle = color;
-      context.fillRect(x, y, width, height);
-
-      // 影线
-      let y1 = priceHeightScale(fLow);
-      let y2 = priceHeightScale(fHigh);
-      let x1 = x + width / 2;
-      let x2 = x1;
-
-      context.beginPath();
-      context.moveTo(x1, y1);
-      context.lineTo(x2, y2);
-      context.closePath();
       context.stroke();
-
+      context.closePath();
       context.restore();
+    } else {
+      for (let i = 0; i < part.length; i++) {
+        context.save();
+        let { fOpen, fClose, fLow, fHigh } = part[i];
+        let color, fill;
+
+        if (fOpen > fClose) {
+          color = GREEN;
+          fill = true;
+        } else {
+          color = RED;
+          fill = false;
+        }
+
+        context.fillStyle = color;
+        context.strokeStyle = color;
+
+        let x = scale(i);
+
+        // 影线
+        let y1 = priceHeightScale(fLow);
+        let y2 = priceHeightScale(fHigh);
+        let x1 = x + rectWidth / 2;
+        let x2 = x1;
+
+        drawLine(x1, y1, x2, y2);
+
+        // 蜡烛
+        let y = priceHeightScale(Math.max(fOpen, fClose));
+        let width = rectWidth;
+        let height = Math.abs(
+          priceHeightScale(fOpen) - priceHeightScale(fClose)
+        );
+        drawRect(x, y, width, height, fill);
+
+        context.restore();
+      }
     }
   };
 
   element.appendChild(canvas);
-  computePriceHeightScale();
 
+  // 绑定缩放事件
   initZoom();
 
-  renderSticks();
+  return {
+    render: renderSticks
+  };
 }
 
 export default KlineChart;
--- src/canvasversion.js
+++ src/canvasversion.js
@@ -1,15 +1,33 @@
-import KlineChart from './canvas_version/KlineChart';
-import axios from 'axios';
+// import KlineChart from './canvas_version/KlineChart';
+// import axios from 'axios';
 
-axios
-  .get('/getkline', {
-    params: {
-      wantNum: 500
-    }
-  })
-  .then(({ data }) => {
-    // console.log(data);
-    KlineChart(document.getElementById('container'), {
-      data: data
-    });
-  });
+// axios
+//   .get('/getkline', {
+//     params: {
+//       wantNum: 500
+//     }
+//   })
+//   .then(({ data }) => {
+//     // console.log(data);
+//     KlineChart(document.getElementById('container'), {
+//       data: data
+//     });
+//   });
+
+const canvas = document.createElement('canvas');
+document.getElementById('container').appendChild(canvas);
+canvas.width = 300;
+canvas.height = 300;
+const context = canvas.getContext('2d');
+// context.globalCompositeOperation = 'source-atop';
+context.beginPath();
+context.moveTo(20, 20);
+context.lineTo(50, 120);
+context.stroke();
+context.closePath();
+
+context.rect(10, 10, 100, 100);
+context.lineCap = 'butt';
+context.fillStyle = '#eee';
+context.stroke();
+context.fill();
